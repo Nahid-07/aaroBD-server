48d9226dbf3398cd8faf5f41c0d8cff07e64cec9
Nahid-07 <nahidasif1998@gmail.com>
2025-11-23 08:42:17 +0600

    modified auth controller and build the delete and create pd system

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,20 +5,19 @@ import generateToken from "../utils/generateToken.js";
 export const registerUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
-
-    // Check if user exists
     const userExists = await User.findOne({ email });
+
     if (userExists) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    // Create new user
     const user = await User.create({ name, email, password });
 
     res.status(201).json({
       _id: user._id,
       name: user.name,
       email: user.email,
+      isAdmin: user.isAdmin, // <--- ADD THIS LINE
       token: generateToken(user._id),
     });
   } catch (error) {
@@ -26,7 +25,7 @@ export const registerUser = async (req, res) => {
   }
 };
 
-// 📌 Login User
+// Login User
 export const loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
@@ -37,6 +36,7 @@ export const loginUser = async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
+        isAdmin: user.isAdmin,
         token: generateToken(user._id),
       });
     } else {
